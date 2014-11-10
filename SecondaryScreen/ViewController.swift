696108c820baf1f7ae412552d853aa428f4d3b60
Aaron Abentheuer <aaron.abenth@icloud.com>
2014-11-10 22:34:20 +0100

    Initial Commit

---
+++ SecondaryScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondaryScreen
+//
+//  Created by Julian Abentheuer on 10.11.14.
+//  Copyright (c) 2014 Aaron Abentheuer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
